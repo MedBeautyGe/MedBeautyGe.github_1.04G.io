ad81665712d59c7bb96b84f7e462b3e66c531cd7
MedBeautyGe <Medbeautyge@hotmail.com>
2025-05-23 15:13:28 +0400

    Add files via upload
    upgrade 1.04 DS

---
+++ lang.js
@@ -0,0 +1,14 @@
+const translations = {
+    ru: {
+        slogan: "СОВЕРШЕНСТВО ДОСТУПНО",
+        // ... остальные переводы
+    },
+    en: {
+        slogan: "PERFECTION IS ACCESSIBLE",
+        // ...
+    },
+    de: {
+        slogan: "VOLLKOMMENHEIT IST ERREICHBAR",
+        // ...
+    }
+};
